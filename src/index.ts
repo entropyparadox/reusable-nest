19b2bf3053aa5eff515d48e3e86789147bfcee51
hugme4944 <hugme4944@gmail.com>
2020-12-31 20:44:17 +0900

    feat: add delete response

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './auth';
 export * from './reusable';
 export * from './base-model.entity';
+export * from './delete-response.dts';
 export * from './reusable-admin.resolver';
 export * from './reusable-users-admin.resolver';
 export * from './reusable-users.service';
