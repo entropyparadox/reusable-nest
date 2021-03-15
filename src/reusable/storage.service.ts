2a0305666e96df60b20405a5613b5c2455670a5b
cycorld <charleschoi87@gmail.com>
2021-03-16 02:08:18 +0900

    feat: resize image

--- src/reusable/storage.service.ts
+++ src/reusable/storage.service.ts
@@ -21,6 +21,8 @@ export class File {
 export class StorageService {
   private readonly bucketName: string;
   private readonly s3: S3;
+  private readonly resize_width: Number;
+  private readonly resize_quality: Number;
 
   constructor(private configService: ConfigService) {
     this.bucketName = this.configService.get<string>('AWS_BUCKET')!;
@@ -29,15 +31,29 @@ export class StorageService {
       accessKeyId: this.configService.get('AWS_ACCESS_KEY'),
       secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
     });
+    this.resize_width = this.configService.get('RESIZE_IMAGE_WIDTH', 768);
+    this.resize_quality = this.configService.get('RESIZE_IMAGE_QUALITY', 80);
   }
 
   private async upload(key: string, file: File | FileUpload) {
+    const fileType = file instanceof File ? file.type : file.mimetype;
+    let fileBuffer = file instanceof File ? file.body : file.createReadStream();
+    if (fileType.includes('image')) {
+      const sharp = require('sharp');
+      fileBuffer = await sharp(
+        file instanceof File ? file.body : file.createReadStream(),
+      )
+        .resize({ width: this.resize_width })
+        .jpeg({ quality: this.resize_quality })
+        .toBuffer();
+    }
+    // TODO :: 파일포멧은 jpg로 통일되지만, 파일 확장자는 원본파일을 따라가고 있음. .jpg를 추가해줄 필요 있음.
     return this.s3
       .upload({
         Bucket: this.bucketName,
         Key: key,
-        Body: file instanceof File ? file.body : file.createReadStream(),
-        ContentType: file instanceof File ? file.type : file.mimetype,
+        Body: fileBuffer,
+        ContentType: fileType,
         ContentEncoding: file instanceof File ? file.encoding : undefined,
       })
       .promise();
