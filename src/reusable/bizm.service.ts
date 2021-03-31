95e9769bbfc5343bd86a46a973ba7740d9e7dcea
hugme4944 <hugme4944@gmail.com>
2021-04-01 00:16:11 +0900

    Add buttons parameter to bizm method

--- src/reusable/bizm.service.ts
+++ src/reusable/bizm.service.ts
@@ -1,32 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
+interface BizmButton {
+  name: string;
+  type: string;
+  url_mobile: string;
+  url_pc: string;
+}
+
 @Injectable()
 export class BizmService {
-  sendMessage(phoneNumber: string, templateId: string, message: string) {
-    axios
-      .post(
-        'https://alimtalk-api.bizmsg.kr/v2/sender/send',
-        [
-          {
-            message_type: 'at',
-            phn: '82' + Number(phoneNumber),
-            profile: process.env.BIZM_PROFILE,
-            tmplId: templateId,
-            msg: message,
-            reserveDt: '00000000000000',
-          },
-        ],
+  sendMessage(
+    phoneNumber: string,
+    templateId: string,
+    message: string,
+    buttons?: BizmButton[],
+  ) {
+    return axios.post(
+      'https://alimtalk-api.bizmsg.kr/v2/sender/send',
+      [
         {
-          headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            userid: process.env.BIZM_USER_ID,
-            Accept: 'application/json',
-          },
+          message_type: 'at',
+          phn: '82' + Number(phoneNumber),
+          profile: process.env.BIZM_PROFILE,
+          tmplId: templateId,
+          msg: message,
+          reserveDt: '00000000000000',
+          ...(buttons?.reduce(
+            (acc, cur, i) => ({
+              ...acc,
+              [`button${i + 1}`]: cur,
+            }),
+            {},
+          ) ?? {}),
         },
-      )
-      .catch(function (err: any) {
-        console.log(err);
-      });
+      ],
+      {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          userid: process.env.BIZM_USER_ID,
+          Accept: 'application/json',
+        },
+      },
+    );
   }
 }
